6a34bfff6908bba1085dc5b888144fe0b00ce77f
JohnDeliso <J_Deliso@yahoo.com>
2020-10-27 13:28:07 -0400

    initial model set up

---
+++ main_app/forms.py
@@ -0,0 +1,3 @@
+from djongo.models import forms
+from django.contrib.auth.models import User
+from .models import *
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import auth
+from django.conf import settings
+from .models import *
 
 # Create your views here.
+# home view
+def home(request):
+    return render(request, 'base.html')
